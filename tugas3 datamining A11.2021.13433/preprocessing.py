e82593ed4eb68797d9fd69c9b0196206b341f88d
JevanBintoro <91891216+JevanBintoro@users.noreply.github.com>
2023-04-04 20:21:30 +0700

    Update preprocessing.py

--- tugas3 datamining A11.2021.13433/preprocessing.py
+++ tugas3 datamining A11.2021.13433/preprocessing.py
@@ -27,7 +27,7 @@ x_train, x_test, y_train, y_test = train_test_split(
     x, y, test_size=0.2, random_state=1)
 sc = StandardScaler()
 x_train[:, 3:] = sc.fit_transform(x_train[:, 3:])
-x[:, 3:] = sc.transform(x[:, 3:])
+x_test[:, 3:] = sc.transform(x_test[:, 3:])
 # print(x)
 print("\n")
 # print(y)
